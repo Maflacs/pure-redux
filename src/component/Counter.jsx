8626d3f7cd398f90d8c4adf42451ed2aaa71977b
Maflacs <effa87@gmail.com>
2024-04-24 19:39:30 +0200

    counter.jsx

---
+++ src/component/Counter.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <div>
+        <p>Counter:</p>
+      </div>
+      <div>
+        <button>Increment</button>
+        <button>Reset</button>
+        <button>Decrement</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
